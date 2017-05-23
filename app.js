5598a2383ddd9a64eb2b2f87fb68eb4b69a4fea1
Aos <aosdab@gmail.com>
2017-05-22 23:11:25 -0500

    Cleaned up

--- app.js
+++ app.js
@@ -13,10 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 const port = process.env.PORT || 3000;
 
 app.listen(port, function(){
-
     console.log(`listening on port: ${port}`);
-
-
 });
 
 let todoArray = [];
