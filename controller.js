d4f33413a7e51b87cdf88e8fda37c3db48748121
Patrick Cleary <vampiirecodes@gmail.com>
2017-06-02 18:17:47 -0400

    huge commit. lots of troubleshooting is behind us. the controller now uses two schemas. one is to define how each todo list item should be formatted. this schema is nested in the list schema which defines a user id and a list array (where the todo list item schema is passed). cleaned up the code and removed unnecessary parts / comments. current state: add works both for the first addition (new user) and additions to current lists (current user)

--- controller.js
+++ controller.js
@@ -1,5 +1,20 @@
 /**
  * Created by Vampiire on 6/1/17.
+
+ PLAN:
+
+ 1) check if a document exists for the given ID (user or channel) - if document exists proceed to 2)
+
+ 1a) if document does not yet exist create it for that given user or channel ID
+
+ 2)
+ add: add a [list item] to that document
+ delete: find the item based on its id [item.id] and remove it from the collection
+ view: iterate through the documents in the collection and display each: "[item.timestampCreated] item.listItem if(item.completed){ [ item.timestampCompleted ] }
+ done: find item based on its id [item.id] and edit item.completed to True
+
+ 3) after each add / delete / done is complete callback view
+
  */
 
 // require the commands file which defines the commands the bot will accept from the user
@@ -9,52 +24,34 @@ const commands = require('./commands');
 const mongoose = require('mongoose');
 
 // connect mongoose to mLab database
-// UN: chingu-todo, PW: todoBOT
 mongoose.connect('mongodb://chingu-todo:todoBOT@ds161121.mlab.com:61121/todo-bot');
 
-// create a schema for what is passed to the database
-let listSchema = new mongoose.Schema({
-    // either channel or user ID will replace the default _id created by the database
-    _id: String,
-    item: {
-        id: Number,
-        listItem: String,
-        timestampCreated: {type: Date, default: Date.now()},
-        completed: {type: Boolean, default: false},
-        timestampCompleted: Date
-    }
+// create a schema for the items in the list array (format for each todo item)
+
+let itemsSchema = new mongoose.Schema({
+
+    _id: {type: Number, default: 0},
+    listItem: String,
+    timestampCreated: {type: Date, default: Date.now()},
+    completed: {type: Boolean, default: false},
+    timestampCompleted: Date
+
 });
 
+// create a schema for each document in the collection. each document (containing the todo list array)
+// is unique to the slack user who calls it. their slack id replaces the default _id passed by mongoDB
 
-/*
-
-PLAN:
-
-1) check if a document exists for the given ID (user or channel) - if document exists proceed to 2)
-
-1a) if document does not yet exist create it for that given user or channel ID
-
-2)
-    add: add a [list item] to that document
-    delete: find the item based on its id [item.id] and remove it from the collection
-    view: iterate through the documents in the collection and display each: "[item.timestampCreated] item.listItem if(item.completed){ [ item.timestampCompleted ] }
-    done: find item based on its id [item.id] and edit item.completed to True
-
-3) after each add / delete / done is complete callback view
-
- */
-
-
+let listSchema = new mongoose.Schema({
+    _id: String,
+    list: [itemsSchema]
+});
 
 
 // create model that implements the list Schema
 let list = mongoose.model('list', listSchema);
 
 
-// // todoArray used for new todo lists DEPRACATED
-//
-// let todoArray = [];
-
+// export the controller back to app.js
 module.exports = function(app) {
 
 // Handle GET requests
@@ -68,92 +65,80 @@ module.exports = function(app) {
     });
 
     function handleQueries(req, res) {
+
         // Validate slack app token
         if (req.token !== "nPJ9nsPaIBb9dv2MDtDRY7sL") {
             console.error("Invalid Token");
             return res.json({text: "Error: Invalid Token"});
         }
 
-        // Get unix timestamp
-        // let timestamp = Math.round(Date.now() / 1000.0);
-
-
-        // check if a collection for that user / channel already exists
-
 
         if (req.text) {
-            let text = req.text;
-            let command = req.text.substring(0, text.indexOf(' '));
-            let listItem = req.text.substring(text.indexOf(' ')+1);
-            let user_id = req.user_id;
-            let exists = false;
-            // let users = text.match(/<@[A-Z0-9]*\|\w*>/g);
 
+        // pull the request sent from slack to the bot. extract the text, and slack user ID
+        // the text will contain both the command (followed by a space) and the string associated with the command
+        // (after the space) as seen by the substrings
 
+            let text = req.text,
+                user_id = req.user_id,
+                command = req.text.substring(0, text.indexOf(' ')),
+                listItem = req.text.substring(text.indexOf(' ')+1);
+
+    // add command is received by the bot
+    // add a new list item
             if (command === 'add') {
 
+            // search for the slack user's list based on their slack ID
                 list.find({_id: user_id}, function(err, doc){
-                    if(doc.length > 0){
-                        // exists = true;
-                        console.log('exists');
-                        console.log(doc);
 
+                // if the user already has a list then push the "added" item into that list and save to the database
+                    if(doc.length > 0){
+                        let length = doc[0].list.length;
                         // modify doc add new item
 
+                        doc[0].list.push({_id: length, listItem : listItem});
 
-                    }
-
-                    else{
-                        let newItem = list(
-                            {_id: user_id,
-                                item: {
-                                    id: 1,
-                                    listItem: listItem,
-                                }
-                            }
-
-                        ).save(function(err){
+                        doc[0].save(function(err){
                             if(err) throw err;
-                            console.log('saved');
+                            console.log('new item added to an existing todo list document array');
                         });
                     }
 
+                // if the user does not already have a list in the collection then create one and pass their slack ID
+                // as well as their list item as the first element in the list array
+                    else{
+
+                         list.create({
+                             _id: user_id,
+                             list: [
+                                 { listItem : listItem }
+                             ]
+                         }, function(err){
+                             if(err) throw err;
+                             console.log('added a new slack user todo list document into the collection');
+
+                         });
+                    }
+
                     });
-
             }
 
-            // Delete todo
+            // Delete a list item
             if (command === 'delete') {
-                // Get todo
-                let todo = listItem;
-                // Delete from array
-                commands.delete(todoArray, todo);
+
             }
 
-            // Complete todo
-            if (command === 'done') {
-                let todo = listItem;
-                let todoTS = timestamp;
-                if (todo = Number(todo)) {
-                    commands.done(todoArray, todo, todoTS)
-                }
-                else {
-                    console.error("Not a valid number");
-                    return res.json({text: "Error: Use `/todo done #` where `#` is todo number"});
-                }
+            // Complete a list item
+            if (command === 'complete') {
+
             }
 
-            // Get list
-            if (command === 'list') {
-                // commands.view(todoArray);
+            // View the entire list
+            if (command === 'view') {
 
-                console.log('comman was list');
-
-                list.find({}, function(err, docs) {
-                    console.log(docs);
-                });
             }
 
+        // return a view back to slack to display to the user
             let data = {
                 response_type: 'ephemeral',
                 attachments: [
