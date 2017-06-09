988b68bfd8ce2f24dbcf9d66d0d7f237b82d5547
Patrick Cleary <vampiirecodes@gmail.com>
2017-06-09 02:57:56 -0400

    removed nonsense

--- controller.js
+++ controller.js
@@ -36,8 +36,6 @@ router.get('/', (req, res) =>{
     res.sendFile(__dirname + '/public/index.html')
 });
 
-router.get('/css.css', )
-
 // Redirect URL
 router.get('/auth/redirect', (req, res) => {
     const options = {
